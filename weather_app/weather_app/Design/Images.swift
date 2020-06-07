ffa2f04e3dd9643dd13f1ccd1a79ad1dbfe03fbd
Anatoly Gurbanov <tomgurbanov@gmail.com>
2020-06-08 04:24:12 +0700

    add images for nav bar buttons

---
+++ weather_app/weather_app/Design/Images.swift
@@ -0,0 +1,20 @@
+//
+//  Images.swift
+//  weather_app
+//
+//  Created by Anatoly on 07.06.2020.
+//  Copyright © 2020 Anatoly. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    public class var cityButtonIcon: UIImage {
+        return #imageLiteral(resourceName: "cityIcon")
+    }
+    
+    public class var locationButtonIcon: UIImage {
+        return #imageLiteral(resourceName: "locationIcon")
+    }
+}
