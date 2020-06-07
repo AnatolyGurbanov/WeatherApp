9114631f1e00b9d04b8b4aa3a18c9c48022efa9f
Anatoly Gurbanov <tomgurbanov@gmail.com>
2020-06-07 15:14:14 +0700

    add new cell for two values from model

---
+++ weather_app/weather_app/Modules/Cells/BlockWeatherDescriptionCell/BlockWeatherDescriptionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BlockWeatherDescriptionTableViewCell.swift
+//  weather_app
+//
+//  Created by Anatoly on 07.06.2020.
+//  Copyright © 2020 Anatoly. All rights reserved.
+//
+
+import UIKit
+
+class BlockWeatherDescriptionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
